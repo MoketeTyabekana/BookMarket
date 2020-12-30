0e0e5dbc47f3589488aae070db2cba93be908b96
EdwardMcFlurry <edward@frienzy-time.co.za>
2020-12-30 13:10:19 +0200

    213038404 - 30 Dec 20 Wed - Modify - Made some space between the Properties of the 'Order' Model class for readability.

--- BookStore/Models/Order.cs
+++ BookStore/Models/Order.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookStore.Models
 {
@@ -12,21 +10,28 @@ namespace BookStore.Models
         {
             OrderDetails = new List<OrderDetails>();
         }
+
         public int Id { get; set; }
+
         [Display(Name = "Order No")]
         public string OrderNo { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         [Display(Name ="Phone No")]
         public string PhoneNo { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         public string Address { get; set; }
 
         public DateTime OrderDate { set; get; }
+
         public virtual List<OrderDetails> OrderDetails { get; set; }
     }
 }
