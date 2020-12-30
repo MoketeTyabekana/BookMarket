88576d8123c5c6430542202d0827e99dfb4f25d1
EdwardMcFlurry <edward@frienzy-time.co.za>
2020-12-30 13:01:03 +0200

    213038404 - 30 Dec 20 Wed - Modify - Cleaned the 'Books' Model class for consistency by organizing and moving few Attributes.

--- BookStore/Models/Books.cs
+++ BookStore/Models/Books.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookStore.Models
 {
@@ -12,60 +8,48 @@ namespace BookStore.Models
         public int Id { get; set; }
 
         [Required]
-        [Display(Name = "Title")]
         public string Title { get; set; }
 
         [Required]
-        [Display(Name = "Author")]
         public string Author { get; set; }
 
         [Required]
-        [Display(Name = "ISBN")]
         public string ISBN { get; set; }
 
         [Required]
-        [Display(Name = "Edition")]
         public string Edition { get; set; }
 
         [Required]
-        [Display(Name = "Description")]
         public string Description { get; set; }
 
         [Required]
         [Display(Name = "Publish Year")]
         public string YearPublished { get; set; }
-
-        
        
-        [Display(Name = "Price")]
-        
-
         public decimal Price { get; set; }
 
         [Required]
         public string Image { get; set; }
 
         [Required]
-        [Display(Name = "Condition")]
         public string Condition { get; set; }
 
         [Required]
         [Display(Name ="Available")]
         public bool IsAvailable { get; set; }
 
-        [Display(Name ="Category")]
         [Required]
+        [Display(Name ="Category")]
         public int ProductTypeId { get; set; }
+
         [ForeignKey("ProductTypeId")]
         public ProductTypes ProductTypes { get; set; }
 
-        [Display(Name = "Special Tag")]
         [Required]
+        [Display(Name = "Special Tag")]
         public int SpecialTagId { get; set; }
+
         [ForeignKey("SpecialTagId")]
         public SpecialTagNames SpecialTagName { get; set; }
-
-
-
     }
 }
