93cb985fac71cd03089fac23055211a283bb8fe3
EdwardMcFlurry <edward@frienzy-time.co.za>
2020-12-30 13:13:00 +0200

    213038404 - 30 Dec 20 Wed - Modify - Made some space between the Properties of the 'SpecialTagNames' Model class for readability.

--- BookStore/Models/SpecialTagNames.cs
+++ BookStore/Models/SpecialTagNames.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
     public class SpecialTagNames
     {
         public int Id { get; set; }
+
         [Required]
         [Display(Name = "Special Tag")]
-
         public string SpecialTagName { get; set; }
     }
 }
-
