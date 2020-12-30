9954cab3f83e025d883e84d01d6a18f6b1fc2b85
EdwardMcFlurry <edward@frienzy-time.co.za>
2020-12-30 13:12:17 +0200

    213038404 - 30 Dec 20 Wed - Modify - Made some space between the Properties of the 'ProductTypes' Model class for readability.

--- BookStore/Models/ProductTypes.cs
+++ BookStore/Models/ProductTypes.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
     public class ProductTypes
     {
         public int Id { get; set; }
+
         [Required]
-        [Display(Name = "Book Category")]
-        
+        [Display(Name = "Book Category")]       
         public string ProductType { get; set; }
     }
 }
